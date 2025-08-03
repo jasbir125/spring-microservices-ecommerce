eca2ccd5b3fa103d50f270715c58edd00141d298
jasbirsingh <jasbir125@gmail.com>
2025-08-03 23:16:46 +0530

    feat(security): Implement mTLS with self-signed certificates for HTTPS
    - Configured Spring Boot to use keystore and truststore for mutual TLS
    - Generated self-signed server and client certificates
    - Enabled HTTPS
    - Verified secure communication using curl with client cert/key

---
+++ auth-service/src/main/java/com/singh/ecommerceapp/configuration/HttpHttpsConfig.java
@@ -0,0 +1,26 @@
+package com.singh.ecommerceapp.configuration;
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpHttpsConfig {
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createHttpConnector());
+        return tomcat;
+    }
+
+    private Connector createHttpConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8090); // your HTTP port
+        connector.setSecure(true);
+        connector.setRedirectPort(8443); // automatically redirects HTTP to HTTPS
+        return connector;
+    }
+}
